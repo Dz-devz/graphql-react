2fc071a714e83e933d0b1a5c4b5e6d58f582ebed
Dz.dev <darwinbjordan@gmail.com>
2025-01-31 13:20:19 +0800

    progress: added update mutation and practicing how it works

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+type User = {
+  id: string;
+  name: string;
+  age: number;
+  isMarried: boolean;
+};
+
 const users = [
   {
     id: "1",
@@ -22,6 +29,23 @@ const users = [
   },
 ];
 
+type GetUserByIdArgs = {
+  id: string;
+};
+
+type CreateUserArgs = {
+  name: string;
+  age: number;
+  isMarried: boolean;
+};
+
+type UpdateUserArgs = {
+  id: string;
+  name?: string;
+  age?: number;
+  isMarried?: boolean;
+};
+
 const typeDefs = `
   type Query {
     getUsers: [User]
@@ -30,6 +54,7 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, age: Int!, isMarried: Boolean!): User
+    updateUser(id: ID!, name: String!, age: Int!, isMarried: Boolean!): User
   }
 
   type User {
@@ -45,21 +70,41 @@ const resolvers = {
     getUsers: () => {
       return users;
     },
-    getUserById: (parent, args) => {
+    getUserById: (
+      _parent: unknown,
+      args: GetUserByIdArgs
+    ): User | undefined => {
       const id = args.id;
       return users.find((user) => user.id === id);
     },
   },
   Mutation: {
-    createUser: (parent, args) => {
-      const { name, age, isMarried } = args;
-      const newUser = {
+    createUser: (_parent: unknown, args: CreateUserArgs): User => {
+      const newUser: User = {
         id: (users.length + 1).toString(),
-        name,
-        age,
-        isMarried,
+        ...args,
       };
       users.push(newUser);
+      return newUser;
+    },
+    updateUser: (_parent: unknown, args: UpdateUserArgs): User => {
+      // let updatedUser: User | undefined = undefined;
+      const { id, name, age, isMarried } = args;
+      let userUpdate: User | undefined;
+
+      users.forEach((user) => {
+        if (user.id === id) {
+          if (name !== undefined) user.name = name;
+          if (age !== undefined) user.age = age;
+          if (isMarried !== undefined) user.isMarried = isMarried;
+          userUpdate = user;
+        }
+      });
+
+      if (!userUpdate) {
+        throw new Error(`User with id ${id} not found`);
+      }
+      return userUpdate;
     },
   },
 };
