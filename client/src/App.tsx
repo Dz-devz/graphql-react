38bdfd0b5d91a2e7b59b3fff31f4c5429fb83584
Dz.dev <darwinbjordan@gmail.com>
2025-02-01 13:51:49 +0800

    progress: connected server to client using reactjs with apollo client

---
+++ client/src/App.tsx
@@ -0,0 +1,134 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useState } from "react";
+import "./App.css";
+
+const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      age
+      name
+      isMarried
+    }
+  }
+`;
+
+const GET_USERS_BY_ID = gql`
+  query GetUserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      age
+      name
+      isMarried
+    }
+  }
+`;
+
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!, $age: Int!, $isMarried: Boolean!) {
+    createUser(name: $name, age: $age, isMarried: $isMarried) {
+      age
+      name
+      isMarried
+    }
+  }
+`;
+
+type user = {
+  id: string;
+  age: number;
+  name: string;
+  isMarried: boolean;
+};
+
+type userData = {
+  age: number;
+  name: string;
+  isMarried: boolean;
+};
+
+function App() {
+  const [users, setUsers] = useState<userData>({
+    name: "",
+    age: 0,
+    isMarried: false,
+  });
+  const {
+    data: getUsers,
+    error: getUsersError,
+    loading: getUsersLoading,
+  } = useQuery(GET_USERS);
+  const {
+    data: getIdData,
+    error: getIdError,
+    loading: getIdLoading,
+  } = useQuery(GET_USERS_BY_ID, {
+    variables: { id: "1" },
+  });
+
+  const [createUser] = useMutation(CREATE_USER);
+
+  if (getUsersLoading || getIdLoading) return <p>Fetching Data...</p>;
+
+  if (getUsersError || getIdError)
+    return <p> Error: {getUsersError?.message || getIdError?.message}</p>;
+
+  async function handleCreateUser() {
+    console.log("Clicked");
+
+    createUser({
+      variables: {
+        name: users.name,
+        age: users.age,
+        isMarried: users.isMarried,
+      },
+    });
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          placeholder="Name"
+          value={users.name}
+          onChange={(e) =>
+            setUsers((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <input
+          placeholder="Age"
+          type="number"
+          value={users.age}
+          onChange={(e) =>
+            setUsers((prev) => ({ ...prev, age: Number(e.target.value) }))
+          }
+        />
+        <button color="white" onClick={handleCreateUser}>
+          Create Users
+        </button>
+      </div>
+      <h1>Users</h1>
+      <p>{getIdData.getUserById.name}</p>
+      <p>{getIdData.getUserById.age}</p>
+      <div>
+        {getUsers.getUsers.map((d: user) => (
+          <div key={d.id}>
+            <h2 className="text-4xl text-red-500">
+              Hi, My name is Whatt?? My name is, whoo? My name is, chka chka,
+              Slim {d.name}
+            </h2>
+            <h2 className="text-4xl text-red-500">
+              Hi, My age is Whatt?? My age is, whoo? My age is, chka chka, Slim{" "}
+              {d.age}
+            </h2>
+            <h3 className="text-4xl text-red-500">
+              {d.isMarried ? "true" : "false"}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
